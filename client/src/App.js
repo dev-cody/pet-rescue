4d95b25008ea4f9d3dbce99dee769fef2c0b27a1
Cody Koscielski <codykoscielski@gmail.com>
2022-05-06 22:37:47 -0400

    Started to setup folders. Made API connection

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React , {useState, useEffect } from 'react'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 
 function App() {
+
+  const [newdata, setNewData] = useState([{}])
+
+  useEffect(() => {
+    fetch("/api")
+    .then(
+        response => response.json()
+    ).then(
+        data => {
+          setNewData(data)
+        }
+    )
+  }, [])
+
   return (
-    <div>App</div>
+    <div>
+      <Header />
+      <Footer />
+    </div>
   )
 }
 
